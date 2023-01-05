e5da4745b52fc9383b08048e502fc876a7ad6700
AlexPavluk <alexanderpavliuk2005@gmail.com>
2023-01-05 01:10:43 +0100

    update enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000/'],
-    allowedHeaders: ['content-type'],
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT || 4200);
 }
