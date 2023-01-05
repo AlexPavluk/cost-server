857b3cbc7e16e8915a4ef3d7655cba0844342556
AlexPavluk <alexanderpavliuk2005@gmail.com>
2023-01-05 01:17:21 +0100

    up origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://costs-client-omega.vercel.app/',
+    allowedHeaders: ['content-type'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 4200);
 }
